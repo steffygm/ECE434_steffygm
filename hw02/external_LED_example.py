32a305bcb3eb0791e6437e01a5037e70f6a4a3f2
steffygm <steffygm@rose-hulman.edu>
2018-09-07 14:32:41 -0400

    Folders hw2/3

---
+++ hw02/external_LED_example.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import Adafruit_BBIO.GPIO as GPIO
+import time
+LED = "USR0"
+delay = 0.25
+
+GPIO.setup(LED, GPIO.OUT)
+
+while 1:
+    GPIO.output(LED, 1)
+    time.sleep(delay)
+    GPIO.output(LED, 0)
+    time.sleep(delay)
