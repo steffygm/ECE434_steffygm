d53c4a70ccb8c5aee94de91b6856b362360cb4c8
steffygm <steffygm@rose-hulman.edu>
2018-09-06 21:34:07 -0400

    hw01 complete

--- hw01/etch-a-sketch.py
+++ hw01/etch-a-sketch.py
@@ -5,24 +5,51 @@ from pygame.locals import *
 
 pygame.init()
 
-screen = pygame.display.set_mode((500, 500))
+width = 500
+height = 500
+screen = pygame.display.set_mode((width, height))
 
-x = 250
-y = 250
+x = width/2
+y = height/2
 
 clock = pygame.time.Clock()
 screen.fill((255,255,255))
+
+speed = 10
 
 while 1:
     clock.tick(60)
     pygame.draw.circle(screen, (0,0,0), (x,y), 2)
     pygame.display.update()
     key = pygame.key.get_pressed()
-    if key[pygame.K_RIGHT]: x+=1
-    if key[pygame.K_LEFT]: x-=1
-    if key[pygame.K_UP]: y-=1
-    if key[pygame.K_DOWN]: y+=1
 
+    # Moving Cursor
+    if key[pygame.K_RIGHT]: x+=(speed/10)
+    if key[pygame.K_LEFT]: x-=(speed/10)
+    if key[pygame.K_UP]: y-=(speed/10)
+    if key[pygame.K_DOWN]: y+=(speed/10)
+
+    # Speed Change
+    if key[pygame.K_w]: speed-=1
+    if key[pygame.K_s]: speed+=1
+    
+    # Speed Check
+    if speed > 100:
+        speed = 100
+    elif speed < 10:
+        speed = 10
+
+    # Overlap
+    if x > width:
+        x = 0
+    if x < 0:
+        x = width
+    if y > height:
+        y = 0
+    if y < 0:
+        y = height
+    
+    # Special Keys
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
