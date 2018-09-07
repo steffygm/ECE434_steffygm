62dc42d6fe4cc5a409ceab0926dffb5ce9f0a802
steffygm <steffygm@rose-hulman.edu>
2018-09-07 14:35:26 -0400

    Git is still weird

--- hw01/etch-a-sketch.py
+++ hw01/etch-a-sketch.py
@@ -5,10 +5,14 @@ from pygame.locals import *
 
 pygame.init()
 
-screen = pygame.display.set_mode((500, 500))
+width = 750
+height = 750
 
-x = 250
-y = 250
+screen = pygame.display.set_mode((width, 750))
+
+x = width/2
+y = height/2
+speed = 10
 
 clock = pygame.time.Clock()
 screen.fill((255,255,255))
@@ -18,10 +22,34 @@ while 1:
     pygame.draw.circle(screen, (0,0,0), (x,y), 2)
     pygame.display.update()
     key = pygame.key.get_pressed()
-    if key[pygame.K_RIGHT]: x+=1
-    if key[pygame.K_LEFT]: x-=1
-    if key[pygame.K_UP]: y-=1
-    if key[pygame.K_DOWN]: y+=1
+
+    # Position Change
+    if key[pygame.K_RIGHT]: x+=(speed/10)
+    if key[pygame.K_LEFT]: x-=(speed/10)
+    if key[pygame.K_UP]: y-=(speed/10)
+    if key[pygame.K_DOWN]: y+=(speed/10)
+
+    # Position Check
+    if x < 0:
+        x = width
+    elif x > width:
+        x = 0
+
+    if y < 0:
+        y = height
+    elif y > height:
+        y = 0
+
+    # Speed Change
+    if key[pygame.K_w]: speed-=1
+    if key[pygame.K_s]: speed+=1
+
+    # Speed Check
+    if speed < 10:
+        speed = 10
+    if speed > 100:
+        speed = 100
+    
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
